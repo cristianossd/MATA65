ab81ada925aea57825e82081708b4e12649c8619
Cristiano Santos <cristianossd@gmail.com>
2016-07-28 22:04:18 -0300

    Applying colors for each side

--- legoBlocks/index.js
+++ legoBlocks/index.js
@@ -5,9 +5,11 @@ var renderer;
 var camera;
 
 function renderScene() {
+  /*
   camera.rotation.x += 0.03;
   camera.rotation.y += 0.02;
   camera.rotation.z += 0.02;
+  */
 
   requestAnimationFrame(renderScene);
   renderer.render(scene, camera);
@@ -28,115 +30,107 @@ function init() {
 
   scene.add( camera );
 
-  var lGeometry = new THREE.Geometry();
+  var vGeometry = new THREE.Geometry();
 
   // block 1
-  lGeometry.vertices.push(new THREE.Vector3( 0.5, -1.0,  0.5));
-  lGeometry.vertices.push(new THREE.Vector3(-0.5, 0.0,  0.5));
-  lGeometry.vertices.push(new THREE.Vector3( 0.5, 0.0,  0.5));
-  lGeometry.vertices.push(new THREE.Vector3(-0.5, -1.0,  0.5));
-  lGeometry.vertices.push(new THREE.Vector3( 0.5, -1.0, -0.5));
-  lGeometry.vertices.push(new THREE.Vector3(-0.5, 0.0, -0.5));
-  lGeometry.vertices.push(new THREE.Vector3( 0.5, 0.0, -0.5));
-  lGeometry.vertices.push(new THREE.Vector3(-0.5, -1.0, -0.5));
+  vGeometry.vertices.push(new THREE.Vector3( 0.5, -1.0,  0.5));
+  vGeometry.vertices.push(new THREE.Vector3(-0.5, 0.0,  0.5));
+  vGeometry.vertices.push(new THREE.Vector3( 0.5, 0.0,  0.5));
+  vGeometry.vertices.push(new THREE.Vector3(-0.5, -1.0,  0.5));
+  vGeometry.vertices.push(new THREE.Vector3( 0.5, -1.0, -0.5));
+  vGeometry.vertices.push(new THREE.Vector3(-0.5, 0.0, -0.5));
+  vGeometry.vertices.push(new THREE.Vector3( 0.5, 0.0, -0.5));
+  vGeometry.vertices.push(new THREE.Vector3(-0.5, -1.0, -0.5));
   // extension 1
-  lGeometry.vertices.push(new THREE.Vector3( 0.5, 1.0, 0.5));
-  lGeometry.vertices.push(new THREE.Vector3(-0.5, 1.0, 0.5));
-  lGeometry.vertices.push(new THREE.Vector3( 0.5, 1.0, -0.5));
-  lGeometry.vertices.push(new THREE.Vector3(-0.5, 1.0, -0.5));
+  vGeometry.vertices.push(new THREE.Vector3( 0.5, 1.0, 0.5));
+  vGeometry.vertices.push(new THREE.Vector3(-0.5, 1.0, 0.5));
+  vGeometry.vertices.push(new THREE.Vector3( 0.5, 1.0, -0.5));
+  vGeometry.vertices.push(new THREE.Vector3(-0.5, 1.0, -0.5));
   // extension 2
-  lGeometry.vertices.push(new THREE.Vector3(1.5, 0.0, 0.5));
-  lGeometry.vertices.push(new THREE.Vector3(1.5, 1.0, 0.5));
-  lGeometry.vertices.push(new THREE.Vector3(1.5, 0.0, -0.5));
-  lGeometry.vertices.push(new THREE.Vector3(1.5, 1.0, -0.5));
+  vGeometry.vertices.push(new THREE.Vector3(1.5, 0.0, 0.5));
+  vGeometry.vertices.push(new THREE.Vector3(1.5, 1.0, 0.5));
+  vGeometry.vertices.push(new THREE.Vector3(1.5, 0.0, -0.5));
+  vGeometry.vertices.push(new THREE.Vector3(1.5, 1.0, -0.5));
 
   // Block Front
-  lGeometry.faces.push(new THREE.Face3(1, 2, 0));
-  lGeometry.faces.push(new THREE.Face3(1, 0, 3));
-  lGeometry.faces[0].materialIndex = 0;
-  lGeometry.faces[1].materialIndex = 0;
+  vGeometry.faces.push(new THREE.Face3(1, 2, 0));
+  vGeometry.faces.push(new THREE.Face3(1, 0, 3));
+  vGeometry.faces[0].materialIndex = 2;
+  vGeometry.faces[1].materialIndex = 2;
 
   // Block Back
-  lGeometry.faces.push(new THREE.Face3(5, 4, 6));
-  lGeometry.faces.push(new THREE.Face3(5, 7, 4));
-  lGeometry.faces[2].materialIndex = 1;
-  lGeometry.faces[3].materialIndex = 1;
+  vGeometry.faces.push(new THREE.Face3(5, 4, 6));
+  vGeometry.faces.push(new THREE.Face3(5, 7, 4));
+  vGeometry.faces[2].materialIndex = 3;
+  vGeometry.faces[3].materialIndex = 3;
 
   // Block Top
-  lGeometry.faces.push(new THREE.Face3(3, 0, 4));
-  lGeometry.faces.push(new THREE.Face3(3, 4, 7));
-  lGeometry.faces[4].materialIndex = 2;
-  lGeometry.faces[5].materialIndex = 2;
-
-  // Block Bottom
-  lGeometry.faces.push(new THREE.Face3(1, 6, 2));
-  lGeometry.faces.push(new THREE.Face3(1, 5, 6));
-  lGeometry.faces[6].materialIndex = 3;
-  lGeometry.faces[7].materialIndex = 3;
+  vGeometry.faces.push(new THREE.Face3(3, 0, 4));
+  vGeometry.faces.push(new THREE.Face3(3, 4, 7));
+  vGeometry.faces[4].materialIndex = 0;
+  vGeometry.faces[5].materialIndex = 0;
 
   // Block Right
-  lGeometry.faces.push(new THREE.Face3(2, 6, 4));
-  lGeometry.faces.push(new THREE.Face3(2, 4, 0));
-  lGeometry.faces[8].materialIndex = 4;
-  lGeometry.faces[9].materialIndex = 4;
+  vGeometry.faces.push(new THREE.Face3(2, 6, 4));
+  vGeometry.faces.push(new THREE.Face3(2, 4, 0));
+  vGeometry.faces[6].materialIndex = 4;
+  vGeometry.faces[7].materialIndex = 4;
 
   // Block Left
-  lGeometry.faces.push(new THREE.Face3(5, 1, 3));
-  lGeometry.faces.push(new THREE.Face3(5, 3, 7));
-  lGeometry.faces[10].materialIndex = 5;
-  lGeometry.faces[11].materialIndex = 5;
+  vGeometry.faces.push(new THREE.Face3(5, 1, 3));
+  vGeometry.faces.push(new THREE.Face3(5, 3, 7));
+  vGeometry.faces[8].materialIndex = 5;
+  vGeometry.faces[9].materialIndex = 5;
 
   // Extension 1
-  lGeometry.faces.push(new THREE.Face3(8, 9, 10));
-  lGeometry.faces.push(new THREE.Face3(11, 10, 9));
-  lGeometry.faces[12].materialIndex = 0;
-  lGeometry.faces[13].materialIndex = 0;
+  vGeometry.faces.push(new THREE.Face3(8, 9, 10));
+  vGeometry.faces.push(new THREE.Face3(11, 10, 9));
+  vGeometry.faces[10].materialIndex = 1;
+  vGeometry.faces[11].materialIndex = 1;
 
-  lGeometry.faces.push(new THREE.Face3(8, 1, 2));
-  lGeometry.faces.push(new THREE.Face3(8, 9, 1));
-  lGeometry.faces[14].materialIndex = 1;
-  lGeometry.faces[15].materialIndex = 1;
+  vGeometry.faces.push(new THREE.Face3(8, 1, 2));
+  vGeometry.faces.push(new THREE.Face3(8, 9, 1));
+  vGeometry.faces[12].materialIndex = 2;
+  vGeometry.faces[13].materialIndex = 2;
 
-  lGeometry.faces.push(new THREE.Face3(11, 5, 10));
-  lGeometry.faces.push(new THREE.Face3(6, 10, 5));
-  lGeometry.faces[16].materialIndex = 2;
-  lGeometry.faces[17].materialIndex = 2;
+  vGeometry.faces.push(new THREE.Face3(11, 5, 10));
+  vGeometry.faces.push(new THREE.Face3(6, 10, 5));
+  vGeometry.faces[14].materialIndex = 3;
+  vGeometry.faces[15].materialIndex = 3;
 
-  lGeometry.faces.push(new THREE.Face3(1, 11, 9));
-  lGeometry.faces.push(new THREE.Face3(1, 5, 11));
-  lGeometry.faces[18].materialIndex = 3;
-  lGeometry.faces[19].materialIndex = 3;
-
-  lGeometry.faces.push(new THREE.Face3(2, 10, 8));
-  lGeometry.faces.push(new THREE.Face3(2, 6, 10));
-  lGeometry.faces[20].materialIndex = 4;
-  lGeometry.faces[21].materialIndex = 4;
+  vGeometry.faces.push(new THREE.Face3(1, 11, 9));
+  vGeometry.faces.push(new THREE.Face3(1, 5, 11));
+  vGeometry.faces[16].materialIndex = 5;
+  vGeometry.faces[17].materialIndex = 5;
 
   // Extension 2
-  lGeometry.faces.push(new THREE.Face3(13, 12, 14));
-  lGeometry.faces.push(new THREE.Face3(13, 14, 15));
-  lGeometry.faces[22].materialIndex = 0;
-  lGeometry.faces[23].materialIndex = 0;
+  vGeometry.faces.push(new THREE.Face3(13, 12, 14));
+  vGeometry.faces.push(new THREE.Face3(13, 14, 15));
+  vGeometry.faces[18].materialIndex = 4;
+  vGeometry.faces[19].materialIndex = 4;
 
-  lGeometry.faces.push(new THREE.Face3(13, 8, 2));
-  lGeometry.faces.push(new THREE.Face3(13, 2, 12));
-  lGeometry.faces[24].materialIndex = 1;
-  lGeometry.faces[25].materialIndex = 1;
+  vGeometry.faces.push(new THREE.Face3(13, 8, 2));
+  vGeometry.faces.push(new THREE.Face3(13, 2, 12));
+  vGeometry.faces[20].materialIndex = 2;
+  vGeometry.faces[21].materialIndex = 2;
 
-  lGeometry.faces.push(new THREE.Face3(10, 14, 15));
-  lGeometry.faces.push(new THREE.Face3(14, 10, 6));
-  lGeometry.faces[26].materialIndex = 2;
-  lGeometry.faces[27].materialIndex = 2;
+  vGeometry.faces.push(new THREE.Face3(10, 14, 15));
+  vGeometry.faces.push(new THREE.Face3(14, 10, 6));
+  vGeometry.faces[22].materialIndex = 3;
+  vGeometry.faces[23].materialIndex = 3;
 
-  lGeometry.faces.push(new THREE.Face3(14, 2, 6));
-  lGeometry.faces.push(new THREE.Face3(14, 12, 2));
-  lGeometry.faces[28].materialIndex = 3;
-  lGeometry.faces[29].materialIndex = 3;
+  vGeometry.faces.push(new THREE.Face3(14, 2, 6));
+  vGeometry.faces.push(new THREE.Face3(14, 12, 2));
+  vGeometry.faces[24].materialIndex = 0;
+  vGeometry.faces[25].materialIndex = 0;
 
-  lGeometry.faces.push(new THREE.Face3(13, 8, 15));
-  lGeometry.faces.push(new THREE.Face3(15, 8, 10));
-  lGeometry.faces[30].materialIndex = 4;
-  lGeometry.faces[31].materialIndex = 4;
+  vGeometry.faces.push(new THREE.Face3(13, 8, 15));
+  vGeometry.faces.push(new THREE.Face3(15, 8, 10));
+  vGeometry.faces[26].materialIndex = 1;
+  vGeometry.faces[27].materialIndex = 1;
+
+  vGeometry.name = 'obj0';
+  vGeometry.center();
 
   //var lMaterial = new THREE.MeshBasicMaterial({color: 0xF6F900, wireframe: true});
 
@@ -149,10 +143,10 @@ function init() {
   	new THREE.MeshBasicMaterial({color:0xFFFFFF, side:THREE.DoubleSide})
   ];
 
-  var lMaterial = new THREE.MeshFaceMaterial(boxMaterials);
-  var lMesh = new THREE.Mesh(lGeometry, lMaterial);
+  var vMaterial = new THREE.MeshFaceMaterial(boxMaterials);
+  var vMesh = new THREE.Mesh(vGeometry, vMaterial);
 
-  scene.add(lMesh);
+  scene.add(vMesh);
 
   document.getElementById('output').appendChild(renderer.domElement);
   renderer.clear();
