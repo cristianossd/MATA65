f9125f16dc167afe5643a740de39e5c223dcea78
Cristiano Santos <cristianossd@gmail.com>
2016-09-18 10:53:50 -0300

    Adding Trackball controls

--- multipleTextures/index.js
+++ multipleTextures/index.js
@@ -1,17 +1,34 @@
 var renderer;
 var scene;
 var camera;
+var controls;
 var texture;
+var clock;
 
 function init() {
+  clock = new THREE.Clock();
 	scene = new THREE.Scene();
 	renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth * 0.9, window.innerHeight * 0.9);
 
 	renderer.setClearColor(new THREE.Color(0.0, 0.0, 0.0));
-  renderer.setSize(600, 600);
 
-	camera = new THREE.OrthographicCamera(-4.0, 4.0, 4.0, -4.0, -4.0, 4.0)
-	scene.add( camera );
+	camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.z = 5;
+
+  controls = new THREE.TrackballControls(camera);
+
+  controls.rotateSpeed = 1.0;
+  controls.zoomSpeed = 1.2;
+  controls.panSpeed = 0.8;
+
+  controls.noZoom = false;
+  controls.noPan = false;
+
+  controls.staticMoving = true;
+  controls.dynamicDampingFactor = 0.3;
+
+  controls.keys = [65, 83, 68];
 
 	// Earth
   var geometry = new THREE.SphereGeometry(2, 30, 30);
@@ -22,11 +39,14 @@ function init() {
 	document.getElementById("output").appendChild(renderer.domElement);
 
   renderer.clear();
-  renderScene();
+  render();
 }
 
-function renderScene() {
-  requestAnimationFrame(renderScene);
+function render() {
+  var delta = clock.getDelta();
+  controls.update(delta);
+
+  requestAnimationFrame(render);
   renderer.render(scene, camera);
 }
 
